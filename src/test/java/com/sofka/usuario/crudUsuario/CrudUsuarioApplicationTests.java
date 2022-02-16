77dfa143f42a7c151cee64dcb3a403fa61dd633c
jcelisca <celiss1997@gmail.com>
2022-02-16 16:15:34 -0500

    CreacÃió del crud de uusuarios con conexión a mysql

---
+++ src/test/java/com/sofka/usuario/crudUsuario/CrudUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.usuario.crudUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
